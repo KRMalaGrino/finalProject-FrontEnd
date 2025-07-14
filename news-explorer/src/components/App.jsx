9bf228601eba4932bff20be30fd31a2512147126
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-13 21:54:10 -0700

    added project skeleton structure

---
+++ news-explorer/src/components/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
