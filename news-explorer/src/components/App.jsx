d6cfa875df7ea5ea09b5e5ea6c3e287b99c54c0b
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-13 23:06:48 -0700

    added all components and skeleton foundations. imported all css to index.css. imported all jsx to app.jsx

--- news-explorer/src/components/App.jsx
+++ news-explorer/src/components/App.jsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
 
 import Header from "./Header/Header";
+import Navigation from "./Navigation/Navigation";
+import SearchForm from "./SearchForm/SearchForm";
+import Main from "./Main/Main";
+import NewsCard from "./NewsCard/NewsCard";
+import About from "./About/About";
 import Footer from "./Footer/Footer";
+import Preloader from "./Preloader/Preloader";
+import LoginModal from "./LoginModal/LoginModal";
+import RegisterModal from "./RegisterModal/RegisterModal";
 
 function App() {
   return (
     <div>
       <Header />
+      <Navigation />
+      <SearchForm />
+      <Main />
+      <NewsCard />
+      <About />
       <Footer />
+      <Preloader />
+      <LoginModal />
+      <RegisterModal />
     </div>
   );
 }
