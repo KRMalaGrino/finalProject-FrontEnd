358ac6c58bbebc1cef4edfc65656c0c84613de48
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-15 15:18:55 -0700

    added styles to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
+
+import githubIcon from "../../images/github-icon.png";
+import facebookIcon from "../../images/facebook-icon.png";
+
 function Footer() {
   return (
-    <div>
-      <h1></h1>
+    <div className="footer">
+      <p className="footer__text">© 2024 Supersite, Powered by News API</p>
+      <div className="footer__link-wrapper">
+        <Link>
+          <button className="footer__link-text" type="button">
+            Home
+          </button>
+        </Link>
+        <Link>
+          <button className="footer__link-text" type="button">
+            TripleTen
+          </button>
+        </Link>
+        <div className="footer__icon-wrapper">
+          <Link>
+            <img className="footer__icon" src={githubIcon} alt="github icon" />
+          </Link>
+          <Link>
+            <img
+              className="footer__icon"
+              src={facebookIcon}
+              alt="facebook icon"
+            />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
