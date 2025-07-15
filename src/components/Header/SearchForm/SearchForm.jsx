e480db930e88cea1403e99a51efc68bef6a1f65c
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-15 07:49:46 -0700

    added search form to the header styles and html

--- src/components/Header/SearchForm/SearchForm.jsx
+++ src/components/Header/SearchForm/SearchForm.jsx
@@ -6,6 +6,12 @@ function SearchForm() {
         Find the latest news on any topic and save them in your personal
         account.
       </p>
+      <form className="searchForm__form">
+        <label className="searchForm__label">
+          <input className="searchForm__input" />
+          <button className="searchForm__btn">Search</button>
+        </label>
+      </form>
     </div>
     // form ?
   );
