f853556b7b3cab7a99e751749aea44e6d2fef080
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-26 16:52:10 -0700

    added news cards . image container css added . next todo is text container css .

--- src/components/Main/NewsCard/NewsCard.jsx
+++ src/components/Main/NewsCard/NewsCard.jsx
@@ -1,11 +1,23 @@
+import bookmarkNormal from "../../../images/bookmark-normal.png";
+
 function NewsCard() {
   return (
     <div className="news-card">
       <div className="news-card__container">
         <div className="news-card__img-container">
-          <img className="news-card__image" src="" alt="" />
-          <img className="news-card__bookmark" src="" alt="" />
-          <img className="news-card__sign-in" src="" alt="" />
+          <img
+            className="news-card__image"
+            src="https://www.channelhouse.com/wp-content/uploads/sites/31/2021/11/Ben-Jones-Bridge-1024x768-1.jpeg"
+            alt="news-picture"
+          />
+          <div className="news-card__bookmark-container">
+            <img
+              className="news-card__bookmark"
+              src={bookmarkNormal}
+              alt="bookmark-normal"
+            />
+          </div>
+          <p className="news-card__sign-in">Sign in to save articles</p>
         </div>
         <div className="news-card__text-container">
           <p className="news-card__date"></p>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,8 @@ const newsData = {
       title: "Breaking: New Tech Trends in 2025",
       description: "A look into the next big trends in tech.",
       url: "https://www.wsj.com/article1",
-      urlToImage: "https://via.placeholder.com/600x400",
+      urlToImage:
+        "https://www.channelhouse.com/wp-content/uploads/sites/31/2021/11/Ben-Jones-Bridge-1024x768-1.jpeg",
       publishedAt: "2025-07-25T10:00:00Z",
       content: "Full article content here...",
     },
@@ -16,9 +17,21 @@ const newsData = {
       title: "Markets Today: What You Need to Know",
       description: "Markets are shifting rapidly due to new regulations.",
       url: "https://www.wsj.com/article2",
-      urlToImage: "https://via.placeholder.com/600x400",
+      urlToImage:
+        "https://media.istockphoto.com/id/1893263632/photo/crashing-waves-on-rock-formations-at-oregon-coast.jpg?s=612x612&w=0&k=20&c=qGY6dChqTrlxUUPJ23Q3K3_Mhe_w94TRICiiYECL9Vg=",
       publishedAt: "2025-07-24T08:30:00Z",
       content: "Market summary article content...",
+    },
+    {
+      source: { id: null, name: "WSJ.com" },
+      author: "Jack Nichols",
+      title: "Guess What?: Jesus is God",
+      description: "The only way . The truth & the light.",
+      url: "https://www.wsj.com/article2",
+      urlToImage:
+        "https://s3.us-west-1.amazonaws.com/lincoln-city/images/Main-Images/_1200x630_crop_center-center_82_none/natural-wonders-main-image-1.jpg?mtime=20190710112350",
+      publishedAt: "2025-07-24T08:30:00Z",
+      content: "See more here...",
     },
   ],
 };
