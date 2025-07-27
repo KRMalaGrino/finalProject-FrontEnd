36208f42804c75c553758e687fe45e0f2fe927fc
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-26 20:33:12 -0700

    added css styling to the news cards.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,17 @@
+import newsData from "../../utils/constants";
 import NewsCard from "./NewsCard/NewsCard";
 
 function Main() {
+  const renderNewsCards = () => {
+    return newsData.articles.map((article, index) => (
+      <NewsCard key={index} article={article} />
+    ));
+  };
+
   return (
     <div className="main">
       <p className="main__title">Search results</p>
-      <div className="main__news-card-container">
-        <NewsCard />
-      </div>
+      <div className="main__news-card-container">{renderNewsCards()}</div>
       <button className="main__show-more-btn">Show more</button>
     </div>
   );
--- src/components/Main/NewsCard/NewsCard.jsx
+++ src/components/Main/NewsCard/NewsCard.jsx
@@ -1,15 +1,19 @@
 import bookmarkNormal from "../../../images/bookmark-normal.png";
 
-function NewsCard() {
+function NewsCard({ article }) {
+  const { urlToImage, title, description, publishedAt, source } = article;
+
+  const formattedDate = new Date(publishedAt).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
   return (
     <div className="news-card">
       <div className="news-card__container">
         <div className="news-card__img-container">
-          <img
-            className="news-card__image"
-            src="https://www.channelhouse.com/wp-content/uploads/sites/31/2021/11/Ben-Jones-Bridge-1024x768-1.jpeg"
-            alt="news-picture"
-          />
+          <img className="news-card__image" src={urlToImage} alt={title} />
           <div className="news-card__bookmark-container">
             <img
               className="news-card__bookmark"
@@ -20,10 +24,10 @@ function NewsCard() {
           <p className="news-card__sign-in">Sign in to save articles</p>
         </div>
         <div className="news-card__text-container">
-          <p className="news-card__date"></p>
-          <p className="news-card__title"></p>
-          <p className="news-card__description"></p>
-          <p className="news-card__source"></p>
+          <p className="news-card__date">{formattedDate}</p>
+          <p className="news-card__title">{title}</p>
+          <p className="news-card__description">{description}</p>
+          <p className="news-card__source">{source.name}</p>
         </div>
       </div>
     </div>
