a55c8199d8e14c32cbef7e242d443f0e0922f4bd
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-28 22:53:48 -0700

    added header switch up for saved-news navigation.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,9 +12,16 @@ function Header({
 }) {
   const location = useLocation();
   const isHomePage = location.pathname === "/";
+  const isSavedArticlesPage = location.pathname === "/saved-articles";
 
   return (
-    <div className="header-searchForm__wrapper">
+    <div
+      className={
+        isSavedArticlesPage
+          ? "header-savedArticles__wrapper"
+          : "header-searchForm__wrapper"
+      }
+    >
       <div className="header">
         <Link to="/" className="header__link">
           <p className="header__title">NewsExplorer</p>
@@ -26,7 +33,14 @@ function Header({
           username={userData?.name}
         />
       </div>
+
       {isHomePage && <SearchForm onSearch={onSearch} />}
+
+      {isSavedArticlesPage && (
+        <div className="header__saved-title-container">
+          <h2 className="header__saved-title">Saved articles</h2>
+        </div>
+      )}
     </div>
   );
 }
