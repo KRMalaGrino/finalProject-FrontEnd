f68cd016b6aa4e6cdbd4ad912ff539bebf821241
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-30 13:51:18 -0700

    main section stretches now

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,22 +1,36 @@
+import { useState } from "react";
+
 import NewsCard from "./NewsCard/NewsCard";
 
 function Main({ articles, handleArticleBookmark }) {
+  const [visibleCount, setVisibleCount] = useState(3);
+
+  const handleShowMore = () => {
+    setVisibleCount((prevCount) => prevCount + 9);
+  };
+
   const renderNewsCards = () => {
-    return articles.map((article, index) => (
-      <NewsCard
-        key={index}
-        article={article}
-        handleArticleBookmark={handleArticleBookmark}
-      />
-    ));
+    return articles
+      .slice(0, visibleCount)
+      .map((article, index) => (
+        <NewsCard
+          key={index}
+          article={article}
+          handleArticleBookmark={handleArticleBookmark}
+        />
+      ));
   };
 
   return (
     <div className="main">
       <p className="main__title">Search results</p>
       <div className="main__news-card-container">{renderNewsCards()}</div>
-      {articles.length > 0 && (
-        <button className="main__show-more-btn" type="button">
+      {visibleCount < articles.length && (
+        <button
+          className="main__show-more-btn"
+          type="button"
+          onClick={handleShowMore}
+        >
           Show more
         </button>
       )}
