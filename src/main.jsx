a661bcdd04ad927c81f5ff093707a26769679530
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-08-17 14:35:04 -0700

    fixed basename in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/finalProject-FrontEnd">
       <App />
     </BrowserRouter>
   </React.StrictMode>
